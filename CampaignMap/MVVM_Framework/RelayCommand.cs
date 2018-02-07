5e33121d84a856ab34ebee9dfbe0101f981c0b94
Fabian <JackAction (Home)>
2018-02-07 18:32:22 +0100

    Campaign: MVVM Framework

---
+++ CampaignMap/MVVM_Framework/RelayCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM_Framework
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException("execute");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #region ICommand Members
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                {
+                    CommandManager.RequerySuggested += value;
+                }
+            }
+            remove
+            {
+                if (_canExecute != null)
+                {
+                    CommandManager.RequerySuggested -= value;
+                }
+            }
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute();
+        }
+
+        #endregion
+    }
+}
